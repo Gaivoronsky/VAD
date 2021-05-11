da08a632a40f3c0085ed06bb3a495b9534b0bcd4
kompotiks <lifami40@gmail.com>
2021-05-11 15:59:47 +0300

    Fix split last chank

--- vad.py
+++ vad.py
@@ -15,7 +15,7 @@ class VAD:
         if not self.mode:
             self.size = self.rate // 4
             self.stop_iter = 5
-            self.threshold_coefficient = 1.2
+            self.threshold_coefficient = 0.8
 
         elif self.mode == 1:
             self.size = self.rate // 2
@@ -27,6 +27,7 @@ class VAD:
             self.stop_iter = 10
             self.threshold_coefficient = 1.1
 
+        self.conter_trec = 0
         self.num_slice = len(self.wav) // self.size
 
         self.audio_frames = []
@@ -35,6 +36,7 @@ class VAD:
         self.sample = 0
         self.counter = 0
         self.start = 0
+        self.detected_pause = 2
 
     def _value_analysis(self):
         self.threshold = np.mean(np.abs(self.wav)) * self.threshold_coefficient
@@ -45,14 +47,17 @@ class VAD:
             self.rec = True
             self.sample += 1
             self.stop_iter = 20
+            self.detected_pause = 2
             self.audio_frames.append(self.data)
         elif self.rec and self.value > self.threshold:
             self.audio_frames.append(self.data)
+            self.detected_pause = 2
         elif self.rec and self.value < self.threshold:
             self.audio_frames.append(self.data)
             self.stop_iter -= 1
+            self.detected_pause -= 1
 
-        if self.rec and self.stop_iter < 0:
+        if self.rec and self.stop_iter < 0 and self.detected_pause < 0:
             self.audio_frames.append(self.data)
             if len(self.audio_frames) > 4:
                 self._write_file()
@@ -60,6 +65,9 @@ class VAD:
                 self.iter_file += 1
                 self.rec = False
                 self.sample = 0
+
+        if self.value == 0 and self.rec:
+            self._write_file()
 
     def scanning(self):
         self._value_analysis()
@@ -71,6 +79,9 @@ class VAD:
             self.value = np.mean(np.abs(self.data))
             self.counter += 1
             self._detected()
+        else:
+            self.value = 0
+            self._detected()
 
     def _write_file(self):
         lenght = len(self.audio_frames)
@@ -80,5 +91,6 @@ class VAD:
 
         time_fragment_start = int(time_start)
         time_fragment_end = int(time_end)
-        fname = f'{time_fragment_start}-{time_fragment_end}.wav'
+        fname = f'{self.conter_trec}.wav'
         sf.write(os.path.join(self.save_dir, fname), list(chain(*self.audio_frames)), self.rate)
+        self.conter_trec += 1
